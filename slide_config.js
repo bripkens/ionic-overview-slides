4187c6f9138d0b867e120f6e9391529547b7567d
Ben Ripkens <bripkens.dev@gmail.com>
2014-12-08 21:07:53 +0100

    init

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Ionic Framework',
+    subtitle: 'An Introduction',
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    favIcon: '../favicon.ico',
+    fonts: [
+    'Open Sans:regular,semibold,italic,italicsemibold',
+    'Source Code Pro'
+    ],
+  },
+
+  presenters: [
+  {
+    name: 'Ben Ripkens',
+    company: 'Developer<br>codecentric AG',
+    gplus: 'http://plus.google.com/107194541655176085264',
+    twitter: '@BenRipkens',
+    www: 'http://bripkens.de',
+    github: 'http://github.com/bripkens'
+  }
+  ]
+};
